2e568deb9c56325a572c7a5cec67463c58f9e235
mrboggii <pruteanbogdan@gmail.com>
2020-05-14 12:30:54 +0200

    starting the backend

---
+++ daou/backend/data.js
@@ -0,0 +1,56 @@
+export default {
+    products: [{
+        _id:'1',
+        name:'Aceite Argán 30ml',
+        image: '/images/30ml.jpg',
+        price:11.99,
+        tipo: 'Bote Cristal',
+        rating: 4,
+        numReviews: 8
+    },
+    {
+        _id:'2',
+        name:'Aceite Argán 50ml',
+        image: '/images/50ml.jpg',
+        price:14.99,
+        tipo: 'Bote Cristal',
+        rating: 4.5,
+        numReviews: 9
+    },
+    {
+        _id:'3',
+        name:'Aceite Argán 100ml',
+        image: '/images/100ml.jpg',
+        price:17.99,
+        tipo: 'Bote Cristal',
+        rating: 5,
+        numReviews: 10
+    },
+    {
+        _id:'4',
+        name:'Pack de 3',
+        image: '/images/pack3.jpg',
+        price:29.99,
+        tipo: 'Bote Cristal',
+        rating: 5,
+        numReviews: 10
+    },
+    {
+        _id:'5',
+        name:'Argán en Plata 30ml',
+        image: '/images/Plata30ml.jpg',
+        price:17.99,
+        tipo: 'Cristal y Plata',
+        rating: 4.5,
+        numReviews: 8
+    },
+    {
+        _id:'6',
+        name:'Argán en Plata 60ml',
+        image: '/images/Plata60ml.jpg',
+        price:19.99,
+        tipo: 'Cristal y Plata',
+        rating: 5,
+        numReviews: 10
+    }]
+}
