d7b79a679c334d76308f4d300589820410064325
mrboggii <pruteanbogdan@gmail.com>
2020-05-15 23:35:14 +0200

    i think i forgot the back

--- daou/backend/config.js
+++ daou/backend/config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/daou'
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/daou',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
