5935a3086307f122bedebb9b5c25af89ca3da2d3
mrboggii <pruteanbogdan@gmail.com>
2020-05-15 20:29:33 +0200

    mongoose + 1st admin created

--- daou/backend/server.js
+++ daou/backend/server.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import data from './data.js';
+import dotenv from 'dotenv';
+import config from './config.js';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute.js';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
 
+
+app.use("/api/users", userRoute);
 app.get("/api/products/:id",(req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x=>x._id === productId);
